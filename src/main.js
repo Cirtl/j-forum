cb89f52efdb636434ef88eb5a42ce48875f6a1dd
Cirtl <2376846435@qq.com>
2022-05-25 18:25:22 +0800

    调整

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-const app = createApp(App)
-
-app.use(router)
-app.use(store)
-app.use(ElementPlus)
-app.mount('#app')
+createApp(App)
+    .use(router)
+    .use(store)
+    .use(ElementPlus)
+    .mount('#app')
