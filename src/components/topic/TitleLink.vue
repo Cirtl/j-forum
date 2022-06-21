5cf82669675404a7041a06c90ec4ef4e5e65c230
Cirtl <2376846435@qq.com>
2022-06-21 17:25:25 +0800

    帖子链接

---
+++ src/components/topic/TitleLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <div @click="lookIt()">{{title}}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        topicId: Number
+    },
+    methods: {
+        lookIt() {
+            const { href } = this.$router.resolve({
+                path: '/post',
+                query: {
+                    id: this.$props.topicId,
+                }
+            })
+            // window.open(href, '_blank')
+            window.open(href, '_self')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
